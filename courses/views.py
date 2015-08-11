efffe6e34d7ad5b709c8a07c30906644af808389
mw4rf <mw4rf@sent.at>
2015-08-11 14:34:46 +0200

    navbar

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Course
+
+def index(request):
+    ''' List of courses. '''
+    courses = Course.objects.order_by('created_at')
+    context = {'courses': courses}
+    return render(request, 'courses/index.html', context)
+    
+def show(request, course_id):
+    ''' Show a course details. '''
+    try:
+        course = Course.objects.get(pk=course_id)
+    except Course.DoesNotExist:
+        raise Http404("This course doesn't exist.")
+    return render(request, 'courses/show.html', {'course': course})

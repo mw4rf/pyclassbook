5d8f53be73e506898922ec85e69c10579afd4070
mw4rf <mw4rf@sent.at>
2015-09-28 12:50:26 +0200

    student list : courses list

--- students/templatetags/students_tags.py
+++ students/templatetags/students_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -10,9 +12,13 @@ def average_for_course(student, course):
 def marks_for_course(student, course):
     return student.marks_for_course(course)
     
-@register.filter
-def format_courses(courses):
+@register.filter(needs_autoescape=True, name="courses_for_student")
+def courses_for_student(courses, autoescape=True):
+    if autoescape:
+        esc = conditional_escape
+    else:
+        esc = lambda x: x
     result = ""
     for c in courses:
-        result = result + c.name + " "
-    return result
+        result = result + '<span class="label label-default"><a href="/courses/%s/">%s</a></span> ' % (esc(c.id), esc(c.name))
+    return mark_safe(result)

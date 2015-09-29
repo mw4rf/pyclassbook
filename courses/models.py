d10053da0a6b0fd370173881aeb7aa5a1030af6f
mw4rf <mw4rf@sent.at>
2015-09-29 11:33:41 +0200

    localization for course model

--- courses/models.py
+++ courses/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 
+from django.utils.translation import ugettext as _
+
 import uuid
 
 class Course(models.Model):
     # UUID
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     # Fields
-    name =          models.CharField(max_length = 254)
-    level =         models.CharField(max_length=10, blank=True)
-    place =         models.CharField(max_length = 254, blank=True)
-    kind =          models.CharField(max_length = 254, blank=True)
-    comments =      models.TextField(blank=True)
-    start_date =    models.DateField(blank=True)
-    end_date =      models.DateField(blank=True)
+    name =          models.CharField(max_length = 254, verbose_name=_('Name'))
+    level =         models.CharField(max_length=10, blank=True, verbose_name=_('Level'))
+    place =         models.CharField(max_length = 254, blank=True, verbose_name=_('Place'))
+    kind =          models.CharField(max_length = 254, blank=True, verbose_name=_('Kind'))
+    comments =      models.TextField(blank=True, verbose_name=_('Comments'))
+    start_date =    models.DateField(blank=True, verbose_name=_('Start date'))
+    end_date =      models.DateField(blank=True, verbose_name=_('End date'))
     # Auto
     created_at =    models.DateTimeField(auto_now_add=True)
     updated_at =    models.DateTimeField(auto_now=True)

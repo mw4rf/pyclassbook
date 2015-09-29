e2137300464a9f47dd5c2eca79f56d23d61f9578
mw4rf <mw4rf@sent.at>
2015-09-29 22:20:56 +0200

    full list of students

--- students/urls.py
+++ students/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'full/^$', views.index_full, name='index_full'),
-    url(r'^(?P<student_id>[^/]+)/$', views.show, name='show')
+    url(r'^full/$', views.index_full, name='index_full'),
+    url(r'^show/(?P<student_id>[^/]+)/$', views.show, name='show')
 ]

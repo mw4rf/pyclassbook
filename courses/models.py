26231760601b2080dd8b408769c9eb9db2ad3879
mw4rf <mw4rf@sent.at>
2015-10-01 15:26:02 +0200

    averages distribution (chart !!!)

--- courses/models.py
+++ courses/models.py
@@ -30,6 +30,31 @@ class Course(models.Model):
     @property
     def exams(self):
         from exams.models import Exam
-        return Exam.objects.filter(course=self.id)
+        return Exam.objects.all().filter(course=self.id)
+    
+    @property
+    def students(self):
+        from students.models import Student
+        students = Student.objects.all().filter(courses__id=self.id)
+        return students
+        
+    @property
+    def averages(self):
+        ''' Returns a list of averages for this course. '''
+        students = self.students
+        averages = [student.average_for_course(self) for student in students]
+        return averages
+        
+    @property
+    def count_averages_occurrences(self):
+        ''' Returns an array with with the number of occurrences for each student's average mark.
+        e.g. {0: 3, 1: 0, 2: 0, 3: 0, 4: 2, 5: 0, ... } : 3 students have 0/20, 2 students have 4/20
+        '''
+        averages = self.averages
+        count = [x for x in range(0,21)]
+        stats = {}
+        for x in count:
+            stats[x] = averages.count(x)
+        return stats
         
 

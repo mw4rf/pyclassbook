8a1ab8972edd5d05ff1e8c804fbba93aba421025
mw4rf <mw4rf@sent.at>
2015-10-04 10:38:23 +0200

    students CSV export

--- students/views.py
+++ students/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+
 from django.http import Http404
+from django.http import HttpResponse
+
 from django.conf import settings
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from django.contrib.auth.decorators import login_required
 
 from .models import Student
@@ -44,3 +48,29 @@ def show(request, student_id):
     except Student.DoesNotExist:
         raise Http404("This student doesn't exist.")
     return render(request, 'students/show.html', {'student': student})
+
+@login_required
+def csv(request):
+    import csv
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="students.csv"'
+
+    writer = csv.writer(response)
+    
+    fields = Student._meta.fields # get fields names
+    students = Student.objects.all().order_by('-created_at')
+    
+    # Output headers
+    headers = []
+    for field in fields:
+        headers.append(field.name)
+    writer.writerow(headers)
+    
+    # Output fields
+    for student in students:
+           row = []
+           for field in fields:
+                row.append(str(getattr(student, field.name)))
+           writer.writerow(row)
+
+    return response

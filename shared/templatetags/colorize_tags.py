863afe0e7cd67a4a25c0314689e67a54a12bfe8a
mw4rf <mw4rf@sent.at>
2015-10-05 12:53:45 +0200

    ui improvements

--- shared/templatetags/colorize_tags.py
+++ shared/templatetags/colorize_tags.py
@@ -5,11 +5,24 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
-@register.filter()
-def colorize_list_group_item(value, threshold):
-    if value < threshold:
-        return "orange inverted"
-    elif value == threshold:
-        return "yellow inverted"
+@register.filter
+def colorize_label_average(avg):
+    if avg > 10:
+        return "green"
+    elif avg == 10:
+        return "orange"
     else:
-        return "olive inverted"
+        return "red"
+        
+@register.filter
+def colorize_label_stddev(stddev):
+    if stddev > 2:
+        return "red"
+    if stddev > 1:
+        return "orange"
+    else:
+        return "green"
+        
+@register.filter
+def colorize_label_mark(mark):
+    return colorize_label_average(mark)

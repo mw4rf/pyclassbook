0c45e17788e35e9d39d7cdfced4b7693aad5cf21
mw4rf <mw4rf@sent.at>
2015-10-08 00:24:09 +0200

    live configuration

--- pyclassbook.py
+++ pyclassbook.py
@@ -9,6 +9,8 @@ if __name__ == "__main__":
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pyclassbook.settings.dev")
     elif sys.argv[1] == "prod":
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pyclassbook.settings.prod")
+    elif sys.argv[1] == "live":
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pyclassbook.settings.live")
     elif sys.argv[1] == "key":
         from django.utils.crypto import get_random_string
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
---
+++ pyclassbook/settings/live.py
@@ -0,0 +1,26 @@
+from .defaults import *
+
+# Pagination
+STUDENTS_PER_PAGE   = 100
+COURSES_PER_PAGE    = 10
+EXAMS_PER_PAGE      = 10
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = open(os.path.expanduser('~/.pyclassbook_secret')).read().strip()
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'pyclassbook.sqlite3'),
+    }
+}
+
+# INTERNATIONALIZATION
+LANGUAGE_CODE = "fr"
+TIME_ZONE = 'Europe/Paris'

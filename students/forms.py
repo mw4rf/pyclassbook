917270a2173a9185819105cd400548cb8df256f2
mw4rf <mw4rf@sent.at>
2016-01-04 17:32:11 +0100

    bug fix

--- students/forms.py
+++ students/forms.py
@@ -4,6 +4,6 @@ from exams.models import Subject
 
 class MarkForm(forms.Form):
     subject = forms.ModelChoiceField(queryset=Subject.objects, to_field_name="id", widget=forms.Select(attrs={'class':'ui search dropdown',}))
-    mark = forms.IntegerField(label='Mark', min_value='0', max_value='20')
+    mark = forms.IntegerField(label='Mark', min_value=0, max_value=20)
     count = forms.BooleanField(label='CountInAverage',required=False, initial=True)
     comment = forms.CharField(label='Comment', widget=forms.TextInput(attrs={'placeholder':'Comment',}))
